257658a521ffdc9dea8c40a0c9b8a820357a43d8
Santiago Miqueo <smiqueo@hotmail.com>
2021-04-13 11:47:34 -0300

    Setting REDUX

--- src/api.js
+++ src/api.js
@@ -32,6 +32,4 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 //Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
 
-const popularGamesUrl = () => `${base_url}${popular_games}`;
-
-console.log(popularGamesUrl());
+export const popularGamesUrl = () => `${base_url}${popular_games}`;
