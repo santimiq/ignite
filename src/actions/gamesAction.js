82e27954cd5ea9e5417fe4bfca684159b482aae8
Santiago Miqueo <smiqueo@hotmail.com>
2021-04-13 13:01:24 -0300

    Fetching other games

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { popularGamesUrl, popularGamesURL } from "../api";
+import { popularGamesUrl, upcomingGamesUrl, newGamesUrl } from "../api";
 
 // Action Creator
 export const loadGames = () => async (dispatch) => {
   //FETCH AXIOS
   const popularData = await axios.get(popularGamesUrl());
+  const newGamesData = await axios.get(newGamesUrl());
+  const upcomingData = await axios.get(upcomingGamesUrl());
   dispatch({
     type: "FETCH_GAMES",
     payload: {
-      popular: popularData.data.result,
+      popular: popularData.data.results,
+      upcoming: upcomingData.data.results,
+      newGames: newGamesData.data.results,
     },
   });
 };
--- src/api.js
+++ src/api.js
@@ -31,5 +31,9 @@ const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 //Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesUrl = () => `${base_url}${popular_games}`;
+export const upcomingGamesUrl = () => `${base_url}${upcoming_games}`;
+export const newGamesUrl = () => `${base_url}${newGames}`;
