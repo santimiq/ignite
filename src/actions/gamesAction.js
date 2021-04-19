4e0eaa9634ba982906b06b836b0dcac47a5ca972
Santiago Miqueo <smiqueo@hotmail.com>
2021-04-19 14:08:29 -0300

    Nav Searching

--- src/actions/gamesAction.js
+++ src/actions/gamesAction.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { popularGamesUrl, upcomingGamesUrl, newGamesUrl } from "../api";
+import {
+  popularGamesUrl,
+  upcomingGamesUrl,
+  newGamesUrl,
+  searchGameURL,
+} from "../api";
 
 // Action Creator
 export const loadGames = () => async (dispatch) => {
@@ -13,6 +18,17 @@ export const loadGames = () => async (dispatch) => {
       popular: popularData.data.results,
       upcoming: upcomingData.data.results,
       newGames: newGamesData.data.results,
+    },
+  });
+};
+
+export const fetchSearch = (game_name) => async (dispatch) => {
+  const searchGames = await axios.get(searchGameURL(game_name));
+
+  dispatch({
+    type: "FETCH_SEARCHED",
+    payload: {
+      searched: searchGames.data.results,
     },
   });
 };
